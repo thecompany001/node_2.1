5fbda0e60fb3907ef352f2e97d7b356c40cee521
the company <thecompany001@protonmail.com>
2021-06-28 18:47:39 -0400

    Node HTTP one

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World</h1></body></html>');
-});
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url;
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        const filePath = path.resolve('./public' + fileUrl)
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html)');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
+}); 
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
