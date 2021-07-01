f231daea05f3baa3aad288512dac7fb33a111dec
the company <thecompany001@protonmail.com>
2021-07-01 17:42:49 -0400

    Node HTTP two

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const { fstat } = require('fs');
 const http = require('http');
+const { pipeline } = require('stream');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,10 +14,29 @@ const server = http.createServer((req, res) => {
             fileUrl = '/index.html';
         }
 
-        const filePath = path.resolve('./public' + fileUrl)
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
     } else {
         res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html)');
+        res.setHeader('Content-Type', 'text/html');
         res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
     }
 }); 
